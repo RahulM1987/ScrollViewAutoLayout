7e483ac85b57071caac30caf063a525ba7f50fe4
Rahul <rahul.love1234@gmail.com>
2018-09-15 17:10:32 +0530

    Initial Commit

--- ScrollVewAutolayout/ViewController.swift
+++ ScrollVewAutolayout/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var txtView: UITextView!
+    @IBOutlet weak var txtViewHeight: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func upDateFrame(_ sender: AnyObject) {
+        self.txtViewHeight.constant = 400
+        self.txtView.layoutIfNeeded()
+        
+    }
 
 }
 
